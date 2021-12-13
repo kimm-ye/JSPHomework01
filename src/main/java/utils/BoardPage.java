b1bb2419f458d10cf488baab64c27b5f878ca746
qoqozb <qoqozb@gmail.com>
2021-12-13 18:58:08 +0900

    2021-12-13 페이징 버튼 수정

--- src/main/java/utils/BoardPage.java
+++ src/main/java/utils/BoardPage.java
@@ -11,27 +11,33 @@ public class BoardPage {
 		int pageTemp = (((pageNum - 1) / blockPage) * blockPage) + 1;
 		
 		if(pageTemp != 1) {
-			pagingStr += "<a href='" + reqUrl + "?pageNum=1'>[처음으로]</a>";
-			pagingStr += "&nbsp;";
-			pagingStr += "<a href='" + reqUrl + "?pageNum=" + (pageTemp - 1) + "'>[이전 페이지]</a>";
+			pagingStr += "<li class='page-item'><a class='page-link' href='" + reqUrl + "?pageNum=1'><i class='bi bi-skip-backward-fill'></i></a>&nbsp;</li>";
+			//pagingStr += "<a href='" + reqUrl + "?pageNum=1'>[처음으로]</a>";
+			//pagingStr += "&nbsp;";
+			pagingStr += "<li class='page-item'><a class='page-link'href='" + reqUrl + "?pageNum=" + (pageTemp - 1) + "'><i class='bi bi-skip-backward-fill'></i></a></li>";
+			//pagingStr += "<a href='" + reqUrl + "?pageNum=" + (pageTemp - 1) + "'>[이전 페이지]</a>";
 		}
 		
 		int blockCount = 1;
 		while(blockCount <= blockPage && pageTemp <= totalPages) {
 			if(pageTemp == pageNum) {
-				pagingStr += "&nbsp;" + pageTemp + "&nbsp;";
+				pagingStr += "<li class='page-item active'><a class='page-link'>&nbsp;"+ pageTemp + "&nbsp;</a></li>";
+				//pagingStr += "&nbsp;" + pageTemp + "&nbsp;";
 			}
 			else {
-				pagingStr += "&nbsp;<a href='" + reqUrl + "?pageNum=" + pageTemp + "'>" + pageTemp + "</a>&nbsp;";
+				pagingStr += "<li class='page-item'><a class='page-link' href='" + reqUrl + "?pageNum=" + pageTemp + "'>&nbsp;" + pageTemp + "&nbsp;</a></li>";
+				//pagingStr += "&nbsp;<a href='" + reqUrl + "?pageNum=" + pageTemp + "'>" + pageTemp + "</a>&nbsp;";
 			}
 			pageTemp++;
 			blockCount++;
 		}
 		
 		if(pageTemp <= totalPages) {
-			pagingStr += "<a href='" + reqUrl +"?pageNum=" + pageTemp + "'>[다음 페이지]</a>";
-			pagingStr += "&nbsp;";
-			pagingStr += "<a href='" + reqUrl + "?pageNum=" + totalPages + "'>[마지막으로]</a>";
+			pagingStr += "<li class='page-item'><a class='page-link' href='" + reqUrl +"?pageNum=" + pageTemp + "'><i class='bi bi-skip-end-fill'></i></a> &nbsp;</li>";
+			//pagingStr += "<a href='" + reqUrl +"?pageNum=" + pageTemp + "'>[다음 페이지]</a>";
+			//pagingStr += "&nbsp;";
+			pagingStr += "<li class='page-item'><a class='page-link' href='" + reqUrl + "?pageNum=" + totalPages + "'><i class='bi bi-skip-forward-fill'></i></a></li>";
+			//pagingStr += "<a href='" + reqUrl + "?pageNum=" + totalPages + "'>[마지막으로]</a>";
 		}
 		
 		return pagingStr;
